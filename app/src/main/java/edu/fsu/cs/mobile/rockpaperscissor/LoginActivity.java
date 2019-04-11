8e63b300ec00b731266e134df94521d6017f6d79
Austin Egan <aegan@novetta.com>
2019-04-11 06:46:00 -0400

    Some things are still busted, and some things are just for testing and will be taken out. Pushing so I can restart my computer

--- app/src/main/java/edu/fsu/cs/mobile/rockpaperscissor/LoginActivity.java
+++ app/src/main/java/edu/fsu/cs/mobile/rockpaperscissor/LoginActivity.java
@@ -1,9 +1,12 @@
 package edu.fsu.cs.mobile.rockpaperscissor;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -14,9 +17,9 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginActivity extends FragmentActivity  {
+public class LoginActivity extends AppCompatActivity  {
 
-    Button login;
+
 
 
     @Override
@@ -24,7 +27,17 @@ public class LoginActivity extends FragmentActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
+        Button login;
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                //check that the username and password are correct, if not, exit this onclick, toast that they are not correct
+                //Toast.makeText(getApplicationContext(), "I'm in the onClick for the login button", Toast.LENGTH_LONG).show();\
+                Log.i("AUSTIN", "login button clicked");
+                Intent i = new Intent(getApplicationContext(), PlayActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
 
